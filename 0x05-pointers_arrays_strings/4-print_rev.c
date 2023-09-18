878e24d474352848a535ac8bb0b9a99ab9005451
root <aminmamine620@gmail.com>
2023-09-18 07:47:25 -0700

    gnius

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int x = sizeof(*s);
+
+	while (*(s + x) != '\0')
+	{
+		_putchar(*(s + x--));
+	}
+	_putchar('\n');
+}
